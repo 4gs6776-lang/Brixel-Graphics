bd20b3fd76f27b0b732f0fcd8efaa9f9da08a318
4gs6776-lang <4gs6776@gmail.com>
2026-07-05 15:54:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -52,3 +52,27 @@ const observer = new IntersectionObserver((entries) => {
 fadeElements.forEach(element => {
     observer.observe(element);
 });
+// ===========================
+// BACK TO TOP BUTTON
+// ===========================
+
+const topBtn = document.getElementById("topBtn");
+
+window.addEventListener("scroll", function () {
+
+    if (window.scrollY > 300) {
+        topBtn.style.display = "block";
+    } else {
+        topBtn.style.display = "none";
+    }
+
+});
+
+topBtn.addEventListener("click", function () {
+
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+
+});
