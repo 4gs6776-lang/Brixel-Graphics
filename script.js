10db1e58437367810fc07c90df161e1dbd0884c4
4gs6776-lang <4gs6776@gmail.com>
2026-07-05 08:34:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const menuToggle = document.getElementById("menu-toggle");
 const navMenu = document.getElementById("nav-menu");
 
+console.log("JavaScript Loaded");
+
 menuToggle.addEventListener("click", () => {
+    console.log("Menu clicked");
     navMenu.classList.toggle("active");
 });
