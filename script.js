ed9170e890d68b6b27c07ae01b1e83b10f7347da
4gs6776-lang <4gs6776@gmail.com>
2026-07-05 15:15:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,15 @@ menuToggle.addEventListener("click", function () {
     }
 
 });
+// ===========================
+// LIGHTBOX
+// ===========================
+
+function openImage(imageSrc){
+    document.getElementById("lightbox").style.display = "flex";
+    document.getElementById("lightbox-img").src = imageSrc;
+}
+
+function closeImage(){
+    document.getElementById("lightbox").style.display = "none";
+}
