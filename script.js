82541907bae714cf0abee350bed41f4554576741
4gs6776-lang <4gs6776@gmail.com>
2026-07-05 15:35:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -35,3 +35,20 @@ document.getElementById("lightbox").addEventListener("click", function(e){
     }
 
 });
+// ===========================
+// SCROLL ANIMATION
+// ===========================
+
+const fadeElements = document.querySelectorAll(".fade-up");
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+            entry.target.classList.add("show");
+        }
+    });
+});
+
+fadeElements.forEach(element => {
+    observer.observe(element);
+});
